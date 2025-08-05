083355289d746d17551c3e24b54dd62169659d4a
VieriElCrack1 <santosvieri9@gmail.com>
2025-08-05 16:24:51 -0500

    feat: aplicando estilos css en el main.jsx

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -2,6 +2,8 @@
 export const FirstApp = () => {
     return (
         <>
+            {/* <h1>{ newMessage.message }</h1> */}
+            {/* <code>{ JSON.stringify(newMessage) }</code> */}
             <h1>Vieri</h1>
             <p>Soy un crack</p>
         </>
