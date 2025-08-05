47cfb579834a6ef45a24ac50ec52f699ec2886af
VieriElCrack1 <santosvieri9@gmail.com>
2025-08-05 17:17:29 -0500

    feat: Event Click (Eventos generales)

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
-export default function CounterApp( { value }) {
+
+export default function CounterApp({ value }) {
+    
+    const handleAdd = () => { 
+        console.log('+1');
+        value = 1000;
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
             <h1>{ value }</h1>
+
+            <button onClick={ handleAdd }>
+                +1
+            </button>
         </>
     )
 }
