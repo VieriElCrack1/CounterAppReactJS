3c59880743fe6e895ff7a44dcf9ce2c63e3d6bbf
VieriElCrack1 <santosvieri9@gmail.com>
2025-08-06 11:04:05 -0500

    feat: Tarea con el hook de useState.

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -3,26 +3,26 @@ import PropTypes from 'prop-types';
 
 
 export default function CounterApp({ value }) {
-    
-    const [ counter, setCounter ] = useState( value );
 
-    const handleAdd = () => {
-        setCounter( counter + 1 );
-        // setCounter( () => counter+1)
-    }
+    console.log('render');
+
+    const [counter, setCounter] = useState(value);
+
+    const handleAdd = () => setCounter(counter + 1);
+    const handleSubstract = () => setCounter(counter - 1);
+    const handleReset = () => setCounter(value);
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h1>{ counter }</h1>
-
-            <button onClick={ handleAdd }>
-                +1
-            </button>
+            <h1>{counter}</h1>
+            <button onClick={handleAdd}> +1 </button>
+            <button onClick={handleSubstract}> -1 </button>
+            <button onClick={handleReset}> Reset </button>
         </>
     )
 }
 
-CounterApp.PropTypes = {
+CounterApp.propTypes = {
     value: PropTypes.number.isRequired
 }
